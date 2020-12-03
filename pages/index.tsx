6bf1b53b81c14787a77c5e1ca582194664c07c28
Bohdan Leonets <leonetsbogdan@gmail.com>
2020-12-03 23:03:54 +0200

    Updated SSR loading and Client loading

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
-import { GetStaticProps } from 'next';
+import { GetServerSideProps } from 'next';
+import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import Layout from '../components/Layout';
 import { Post } from '../interfaces';
@@ -8,7 +9,23 @@ export interface PostProps {
   posts: Post[];
 }
 
-export default function IndexPage({ posts }: PostProps) {
+export default function IndexPage({ posts: serverPosts }: PostProps) {
+  const [posts, setPosts] = useState(serverPosts);
+
+  useEffect(() => {
+    async function load() {
+      const response = await fetch('https://simple-blog-api.crew.red/posts');
+      const data = await response.json();
+      setPosts(data);
+    }
+
+    if (!posts) {
+      load();
+    }
+  }, [])
+
+  console.log(posts);
+  
   return (
     <Layout title="Blog | Home | Posts">
       <p>
@@ -21,7 +38,13 @@ export default function IndexPage({ posts }: PostProps) {
   );
 }
 
-export const getStaticProps: GetStaticProps = async () => {
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  if (!req) {
+    return {
+      props: { posts: null },
+    }
+  }
+
   const response = await fetch('https://simple-blog-api.crew.red/posts');
   const posts = await response.json();
 
--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -1,14 +1,40 @@
-import { GetStaticProps, GetStaticPaths } from 'next';
+import { GetServerSideProps } from 'next';
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
 import Layout from '../../components/Layout';
 import Link from 'next/link';
 import { Post } from '../../interfaces';
 import { Comments } from '../../components/Comments';
 
-export interface PostProps{
+export interface PostProps {
   post: Post;
+  errors?: string;
 }
  
-export default function PostDetail({ post }: PostProps) {
+export default function PostDetail({ post: serverPost }: PostProps) {
+  const [post, setPost] = useState(serverPost);
+  const router = useRouter();
+
+  useEffect(() => {
+    async function load() {
+      const response = await fetch(`https://simple-blog-api.crew.red/posts/${router.query.id}?_embed=comments`);
+      const data = await response.json();
+      setPost(data);
+    }
+
+    if (!serverPost) {
+      load();
+    }
+  }, []);
+
+  if (!serverPost) {
+    return (
+      <Layout>
+        <p>Loading ...</p>
+      </Layout>
+    );
+  }
+
   return (
     <Layout title={`Blog | Post#${post.id}`}>
       <h1>
@@ -16,36 +42,26 @@ export default function PostDetail({ post }: PostProps) {
       </h1>
       <p>{post.title}</p>
       <p>{post.body}</p>
-      <Comments comments={post.comments} id={post.id}/>
-      <Link href='/'>
+      <Comments comments={post.comments} id={post.id} />
+      <Link href="/">
         <a>Back to all posts</a>
       </Link>
     </Layout>
   );
 };
 
-export const getStaticPaths: GetStaticPaths = async () => {
+export const getServerSideProps: GetServerSideProps = async ({ params, req }) => {
+  const id = params?.id;
+  const response = await fetch(`https://simple-blog-api.crew.red/posts/${id}?_embed=comments`);
+  const post = await response.json();
 
-  const response = await fetch(`https://simple-blog-api.crew.red/posts`);
-  const posts = await response.json();
-
-  const paths = posts.map((post: Post) => ({
-    params: { id: post.id.toString() },
-  }));
+  if (!req) {
+    return {
+      props: { post: null}
+    }
+  }
 
   return {
-    paths, fallback: false
+    props: { post }
   };
-};
-
-export const getStaticProps: GetStaticProps = async ({ params }) => {
-  try {
-    const id = params?.id;
-    const response = await fetch(`https://simple-blog-api.crew.red/posts/${id}?_embed=comments`);
-    const post = await response.json();
-
-    return { props: { post } };
-  } catch (err) {
-    return { props: { errors: err.message } };
-  }
 };
